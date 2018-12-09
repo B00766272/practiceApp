3b2a3712d5ef7672691586f71cc9e7fd83cf48e4
B00766272 <Vallescas-A@ac.uk>
2018-12-09 21:15:52 +0000

    changes in grid layout

--- exampleApp/src/main/java/ie/allen/MyUI.java
+++ exampleApp/src/main/java/ie/allen/MyUI.java
@@ -35,34 +35,38 @@ public class MyUI extends UI {
 			  "loginTimeout=30;";
 
 
-        final HorizontalLayout layout = new HorizontalLayout();
-        layout.setMargin(true);
+        final VerticalLayout layout = new VerticalLayout();//masterLayout
         HorizontalLayout cell1 = new HorizontalLayout();
         HorizontalLayout cell2 = new HorizontalLayout();
-        HorizontalLayout gridlayout = new HorizontalLayout();
+               HorizontalLayout gridlayout = new HorizontalLayout();
+        gridlayout.setDefaultComponentAlignment(Alignment.MIDDLE_RIGHT);
+        gridlayout.setSizeFull();
         VerticalLayout v = new VerticalLayout();
+        VerticalLayout v1= new VerticalLayout();
+    
         //content of Cell1
         Label logo = new Label("<H1>Marty Party Planners</H1> <p/> <h3>Please enter the details below and click Book</h3>", ContentMode.HTML);
         
-        //content of cell2
-        final TextField nameOfParty = new TextField();
-        nameOfParty.setCaption("Name of Party");
+       //content of cell2
+       final TextField nameOfParty = new TextField();
+       nameOfParty.setCaption("Name of Party");
 
-        Slider s = new Slider("Value", 1, 250);
-        s.setValue(200.0);
-        s.setWidth(s.getMax()+"px");
+       Slider s = new Slider("Value", 1, 250);
+       s.setValue(200.0);
+       s.setWidth(s.getMax()+"px");
+
    
-        
-        ComboBox<String> children = new ComboBox<String>("Children Attending?");
-        children.setItems("yes", "no");
 
-        //vertical layout content
+       ComboBox<String> children = new ComboBox<String>("Children Attending?");
+       children.setItems("yes", "no");
 
-        Button bookButton = new Button("Book");
+       //vertical layout content
 
+       Button bookButton = new Button("Book");
+       Label message = new Label("<h3>Your party is not booked yet</h3>", ContentMode.HTML);
+    
 
-
-
+    
 
         try 
         {
@@ -70,7 +74,7 @@ public class MyUI extends UI {
         connection = DriverManager.getConnection(connectionString);
         // Add a label to the web app with the message and name of the database we connected to 
         //to check the database run the layout below
-         layout.addComponent(new Label("Connected to database: " + connection.getCatalog()));
+         //layout.addComponent(new Label("Connected to database: " + connection.getCatalog()));
         //query below - as loop:
         ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM PartyRooms;");
         // Convert the resultset that comes back into a List - we need a Java class to represent the data (Customer.java in this case)
@@ -89,16 +93,21 @@ public class MyUI extends UI {
             Grid<PartyRooms> myGrid = new Grid<PartyRooms>();
             // Set the items (List)
              myGrid.setItems(rooms);
+             myGrid.setSizeFull();
             // Configure the order and the caption of the grid
              myGrid.addColumn(PartyRooms::getRooms).setCaption("Rooms");
              myGrid.addColumn(PartyRooms::getCapacity).setCaption("Capacity");
              myGrid.addColumn(PartyRooms::getFeature).setCaption("Feature");
-             myGrid.addColumn(PartyRooms::getAlcoholAllowed).setCaption("Alcohol_Allowed");
+             myGrid.addColumn(PartyRooms::getAlcoholAllowed).setCaption("Alcohol Allowed");
              myGrid.setSelectionMode(SelectionMode.MULTI);
+            
             
 
             // Add the grid to the list
             gridlayout.addComponent(myGrid);
+
+
+
         
        }
        catch (Exception e) 
@@ -107,12 +116,17 @@ public class MyUI extends UI {
         gridlayout.addComponent(new Label(e.getMessage()));
        }
 
+
+        
+
+
      
         cell1.addComponent(logo);
         cell2.addComponents( nameOfParty,s,children);
-       
-        v.addComponents( cell1,cell2,bookButton, gridlayout);
-        layout.addComponent(v);
+      
+        v1.addComponent(message );
+        v.addComponents( cell1,cell2,bookButton,v1);
+        layout.addComponents(v,gridlayout);
        
 
         
